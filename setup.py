a5976f3f83d3f122173fdec3a282f070dc725340
Deepak Singh <ma23m006@smail.iitm.ac.in>
2024-06-11 10:18:53 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # we use this setup file for installing a local package in my virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    versions='0.0.1',
+    author='Deepak Singh',
+    author_email='ma23m006@smail.iitm.ac.in',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
